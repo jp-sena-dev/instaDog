7b206fe7951c6fb0276109d6ce39780b1d56065b
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-11-06 14:47:52 -0300

    update: component ShowImage

--- src/components/organisms/ShowImage/index.js
+++ src/components/organisms/ShowImage/index.js
@@ -1,19 +1,15 @@
-import React, { useState } from 'react';
-import { useImages } from '../../../context/ImagesContext';
+import React from 'react';
 import MainImage from '../../molecules/MainImage';
 import HeaderImage from '../../molecules/HeaderImage';
 import FooterImage from '../../molecules/FooterImage';
 import { SImageDiv } from './styles';
 
 export default function ShowImage({ image }) {
-  const { savedList } = useImages();
-  const [imageUrl] = useState(savedList[image]);
-
   return (
     <SImageDiv>
       <HeaderImage />
-      <MainImage imageUrl={imageUrl} />
-      <FooterImage imageUrl={imageUrl} />
+      <MainImage imageUrl={image.replace(/&/g, '/')} />
+      <FooterImage imageUrl={image.replace(/&/g, '/')} />
     </SImageDiv>
   );
 }
