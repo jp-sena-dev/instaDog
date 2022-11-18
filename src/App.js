1c51b732cbbce3c4fce146e1d0cc197d0102b6ea
joaopedr0sena <joaopedrosena1997@gmail.com>
2022-11-18 08:39:28 -0300

    add: modal on app

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import { Route, Routes } from 'react-router-dom';
 import * as Page from './pages';
 import ImagesProvider from './context/ImagesContext';
 import { GlobalStyle } from './styles/global';
+import Share from './components/organisms/ShareModal';
+import { ShareProvider } from './context/ShareModalContext';
 
 export default function App() {
   return (
     <ImagesProvider>
-      <Routes>
-        <Route path="/showImage/:image" element={<Page.ShowImages />} />
-        <Route path="/salvos" element={<Page.Saved />} />
-        <Route path="/" element={<Page.Home />} />
-      </Routes>
-      <GlobalStyle />
+      <ShareProvider>
+        <Share />
+        <Routes>
+          <Route path="/showImage/:image" element={<Page.ShowImages />} />
+          <Route path="/salvos" element={<Page.Saved />} />
+          <Route path="/" element={<Page.Home />} />
+        </Routes>
+        <GlobalStyle />
+      </ShareProvider>
     </ImagesProvider>
   );
 }
